3d16abf3a7cd2afad7b9d72ad19f17bc92077bdc
amanvrpi <162115205+amanvrpi@users.noreply.github.com>
2024-03-26 10:30:40 +0530

    update db

--- src/main/java/com/vrpigroup/usermodule/UserModuleApplication.java
+++ src/main/java/com/vrpigroup/usermodule/UserModuleApplication.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
@@ -46,7 +47,7 @@ license = @License(
 )
 
 @EnableJpaRepositories
-public class UserModuleApplication {
+public class UserModuleApplication extends SpringBootServletInitializer {
 
     @Bean
     public RestTemplate restTemplate() {
